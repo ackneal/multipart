d0435c293543f72eb09146a8fd2ac172a3923b79
NealH <neal.anglo@gmail.com>
2020-04-23 21:36:45 +0800

    feat: add methods to check boundary

--- src/Stream.php
+++ src/Stream.php
@@ -13,6 +13,7 @@ class Stream implements StreamInterface
     use StreamDecoratorTrait;
 
     private $stream;
+    private $nl;
     private $boundary;
 
     public function __construct($resource, string $boundary)
@@ -25,5 +26,31 @@ class Stream implements StreamInterface
             'dashPrefix' => '--' . $boundary,
             'dashEnclose' => '--' . $boundary . '--',
         ];
+    }
+
+    private function isDelimiter(string $line): bool
+    {
+        if (strpos($line, $this->boundary['dashPrefix']) !== 0) {
+            return false;
+        }
+        $rest = $this->ltrimLWSP(substr($line, strlen($this->boundary['dashPrefix'])));
+        if (is_null($this->nl) and preg_match("/^\r?\n$/", $rest) === 1) {
+            $this->nl = $rest;
+        }
+        return $rest === $this->nl;
+    }
+
+    private function isFinalBoundary(string $line): bool
+    {
+        if (strpos($line, $this->boundary['dashPrefix']) !== 0) {
+            return false;
+        }
+        $rest = $this->ltrimLWSP(substr($line, strlen($this->boundary['dashEnclose'])));
+        return strlen($rest) === 0 or $rest === $this->nl;
+    }
+
+    private function ltrimLWSP(string $line): string
+    {
+        return ltrim($line, " \t");
     }
 }
